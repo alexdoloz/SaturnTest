df7354de00744d82c743599b87ad84be54e1bbea
Alexander Doloz <doloz.alexander@gmail.com>
2015-12-24 23:35:56 +0300

    Start developing login manager

---
+++ SaturnTest/LoginManager.swift
@@ -0,0 +1,24 @@
+//
+//  LoginManager.swift
+//  SaturnTest
+//
+//  Created by Alexander on 24.12.15.
+//  Copyright © 2015 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class LoginManager: NSObject {
+//    private var url: NSURL!
+    private var socket: SRWebSocket!
+    private var sequenceId: NSUUID!
+    
+    init(url: NSURL) {
+        socket = SRWebSocket(URL: url)
+        sequenceId = NSUUID()
+    }
+    
+    func performLogin(email: String, password: String, completion: (NSError?) -> Void) {
+        
+    }
+}
