bb1bcdef8a11dc42d4eb3264f123266ca8e87763
Alexander Doloz <doloz.alexander@gmail.com>
2015-12-25 01:01:21 +0300

    Start implementing and testing Login Manager

--- SaturnTest/LoginManager.swift
+++ SaturnTest/LoginManager.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 
-class LoginManager: NSObject {
-//    private var url: NSURL!
+class LoginManager: NSObject, SRWebSocketDelegate {
+// MARK: Constants
+    let LOGIN_CUSTOMER = "LOGIN_CUSTOMER"
+    let EMAIL_KEY = "email"
+    let PASSWORD_KEY = "password"
+    
+// MARK: Variables
     private var socket: SRWebSocket!
-    private var sequenceId: NSUUID!
+    private var sequenceId: String
+    
+    private var isSocketOpened = false
     
     init(url: NSURL) {
         socket = SRWebSocket(URL: url)
-        sequenceId = NSUUID()
+        sequenceId = NSUUID().UUIDString
+        super.init()
+        socket.delegate = self
+        socket.open()
+        
     }
     
-    func performLogin(email: String, password: String, completion: (NSError?) -> Void) {
-        
+    deinit {
+        socket.delegate = nil
+        socket.close()
+    }
+    
+    func performLogin(email email: String, password: String, completion: (NSError?) -> Void) {
+        guard isSocketOpened else { return }
+        let message = Message(type: LOGIN_CUSTOMER, sequenceId: sequenceId, dataObject: [
+            EMAIL_KEY : email,
+            PASSWORD_KEY : password
+        ])
+        socket.send(message.jsonData)
+    }
+    
+// MARK: SRWebSocketDelegate
+    func webSocketDidOpen(webSocket: SRWebSocket!) {
+        isSocketOpened = true
+        print("Socket did open")
+    }
+    
+    func webSocket(webSocket: SRWebSocket!, didFailWithError error: NSError!) {
+        print("Socket failed with error: \(error)")
+    }
+    
+    func webSocket(webSocket: SRWebSocket!, didReceiveMessage message: AnyObject!) {
+        print("Got message! \(message)")
+    }
+    
+    func webSocket(webSocket: SRWebSocket!, didCloseWithCode code: Int, reason: String!, wasClean: Bool) {
+        print("Code: \(code)")
     }
 }
--- SaturnTest/LoginViewController.swift
+++ SaturnTest/LoginViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import TSValidatedTextField
 
 
 class LoginViewController: UIViewController {
+    var manager: LoginManager!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let message = Message(type: "LOGIN_CUSTOMER", sequenceId: "a29e4fd0-581d-e06b-c837-4f5f4be7dd18", dataObject: ["email" : "fb@bk.ru", "password" : "123123"])
         print("\(message.jsonData)")
         print("\(NSString(data: message.jsonData, encoding: NSUTF8StringEncoding)))")
+        let testURLString = "ws://95.213.131.42:8080/customer-gateway/customer"
+        let url = NSURL(string: testURLString)!
+        manager = LoginManager(url: url)
+        manager.performLogin(email: "fb@bk.ru", password: "123123") { error in
+        }
+    }
+    
+    @IBOutlet weak var emailTextField: TSValidatedTextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func performLogin(sender: AnyObject) {
+        manager.performLogin(email: emailTextField.text!, password: passwordTextField.text!) { (error) -> Void in
+            
+        }
     }
 }
 
