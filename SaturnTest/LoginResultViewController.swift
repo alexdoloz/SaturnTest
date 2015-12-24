b98808b7eec2f34e53ed0382e6a8c01e68e36175
Alexander Doloz <doloz.alexander@gmail.com>
2015-12-24 22:40:17 +0300

    Create Login and LoginResult screens

---
+++ SaturnTest/LoginResultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginResultViewController.swift
+//  SaturnTest
+//
+//  Created by Alexander on 24.12.15.
+//  Copyright © 2015 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class LoginResultViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ SaturnTest/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SaturnTest
+//
+//  Created by Alexander on 24.12.15.
+//  Copyright © 2015 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
